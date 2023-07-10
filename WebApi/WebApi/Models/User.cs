21b8e8ffd189a4ab67820fb50811eda7348457e9
funny <funny154@gamil.com>
2023-07-10 15:43:22 +0800

    feat :  加上ef core的 models

---
+++ WebApi/WebApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Phone { get; set; }
+}
--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,9 +1,15 @@
+﻿
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<IncrudContext, IncrudContext>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
